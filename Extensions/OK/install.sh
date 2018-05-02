7546e10d3d3fe4659be489263346dd21ad0100e1
Nicolas Barral <taupalosaurus@users.noreply.github.com>
2018-05-02 18:21:50 +0100

    simplify install script to use conda env

--- Extensions/OK/install.sh
+++ Extensions/OK/install.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 # Install OK.py
 
-export HOME=/home/$1
-cd $HOME
-
-echo "THE HOME VARIABLE IS SET THERE: "
-echo $HOME
-
-ENV_NAME=okpyenv2
-
-python3 -m venv $ENV_NAME --without-pip
-source $ENV_NAME/bin/activate
-curl https://bootstrap.pypa.io/get-pip.py | python
-deactivate
-
-source $ENV_NAME/bin/activate
-wget https://raw.githubusercontent.com/Cal-CS-61A-Staff/ok-client/master/requirements.txt
-pip install -r requirements.txt
+source /anaconda/bin/activate root
+#wget https://raw.githubusercontent.com/Cal-CS-61A-Staff/ok-client/master/requirements.txt
+#pip install -r requirements.txt
 pip install -U okpy
 
 #test
 python -c "from client.api.notebook import Notebook"
-deactivate
