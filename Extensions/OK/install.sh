33f4602a66ec530009e93aa21e7599d4f3fbfa7f
Nicolas Barral <taupalosaurus@users.noreply.github.com>
2018-05-02 18:54:09 +0100

    new try

--- Extensions/OK/install.sh
+++ Extensions/OK/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Install OK.py
 
-conda create -n okpyenv python=3.5
-source /anaconda/bin/activate okpyenv
+#conda create -n okpyenv python=3.5
+#source /anaconda/bin/activate okpyenv
 #wget https://raw.githubusercontent.com/Cal-CS-61A-Staff/ok-client/master/requirements.txt
 #pip install -r requirements.txt
-pip install -U okpy
+pip3 install -U okpy
 
 #test
-python -c "from client.api.notebook import Notebook"
+python3 -c "from client.api.notebook import Notebook"
