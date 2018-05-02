aa4d9f1b174555214b27393e5d57cbabf800b4d1
Nicolas Barral <taupalosaurus@users.noreply.github.com>
2018-05-02 18:34:18 +0100

    revert

--- Extensions/OK/install.sh
+++ Extensions/OK/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Install OK.py
 
-source /anaconda/bin/activate py35
+source /anaconda/bin/activate root
 #wget https://raw.githubusercontent.com/Cal-CS-61A-Staff/ok-client/master/requirements.txt
 #pip install -r requirements.txt
 pip install -U okpy
