f2501adc1fd30d71bba6a32e453d37fc064cefe4
Nicolas Barral <n.barral@imperial.ac.uk>
2018-05-02 15:47:42 +0100

    Edit deployment and install files

--- Extensions/OK/install.sh
+++ Extensions/OK/install.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 # Install OK.py
 
-source /anaconda/bin/activate root
+ENV_NAME=okpyenv2
+
+python3 -m venv $ENV_NAME --without-pip
+source $ENV_NAME/bin/activate
+curl https://bootstrap.pypa.io/get-pip.py | python
+deactivate
+
+source $ENV_NAME/bin/activate
+wget https://raw.githubusercontent.com/Cal-CS-61A-Staff/ok-client/master/requirements.txt
+pip install -r requirements.txt
 pip install -U okpy
+
+#test
+python -c "from client.api.notebook import Notebook"
+deactivate
