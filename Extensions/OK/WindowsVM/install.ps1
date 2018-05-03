f3a759aaf3fcf7e6c68756a776853ac5b17e52f2
Tempest van Schaik <TempestVanSchaik@users.noreply.github.com>
2018-05-03 17:39:16 +0100

    works to deploy an empty (non data science) VM

--- Extensions/OK/WindowsVM/install.ps1
+++ Extensions/OK/WindowsVM/install.ps1
@@ -1,11 +1,7 @@
-
-
 conda create --name OKenv
 activate OKenv
 conda install pip
 pip install -r https://raw.githubusercontent.com/Cal-CS-61A-Staff/ok-client/master/requirements.txt
 conda install ipykernel
 python -m ipykernel install --user --name myenv --display-name "OKenv"
-
 conda install jupyter
-jupyter notebook #make sure to select Kernel>change kernel and select OKenv
