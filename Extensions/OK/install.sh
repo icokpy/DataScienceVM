1006f573affeaabcbae873eb55b5a666168a1b0f
Nicolas Barral <taupalosaurus@users.noreply.github.com>
2018-05-04 15:28:56 +0100

    simplify install script using standard conda env

--- Extensions/OK/install.sh
+++ Extensions/OK/install.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
 # Install OK.py
-username="${1}"
 
-if [ -z "${username}" ]; then
-  echo "no username :(" >> /tmp/debug-deploy
-  exit 1
-else
-  echo "username: ${username}" >> /tmp/debug-deploy
-fi
-
-venv_path="/home/${username}/okpyenv2"
-
-python3 -m venv "${venv_path}" --without-pip
-
-curl https://bootstrap.pypa.io/get-pip.py | "${venv_path}/bin/python"
-
-"${venv_path}/bin/pip" install okpy
-
-"${venv_path}/bin/python" -c "from client.api.notebook import Notebook"
-
-chown -R $username $venv_path
+/anaconda/envs/py35/bin/pip install -U okpy
