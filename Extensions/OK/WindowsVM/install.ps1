588583ed41495c1e05be1d9e9bd977ef2ae024c1
Tempest van Schaik <TempestVanSchaik@users.noreply.github.com>
2018-05-04 09:45:34 +0100

    add -y for silent install

--- Extensions/OK/WindowsVM/install.ps1
+++ Extensions/OK/WindowsVM/install.ps1
@@ -1,7 +1,7 @@
 conda create --name OKenv
 activate OKenv
-conda install pip
+conda install -y pip
 pip install -r https://raw.githubusercontent.com/Cal-CS-61A-Staff/ok-client/master/requirements.txt
-conda install ipykernel
+conda install -y ipykernel
 python -m ipykernel install --user --name myenv --display-name "OKenv"
-conda install jupyter
+conda install -y jupyter
