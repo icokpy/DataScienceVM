0350abb1628c6383786e4a687f8a45b93b5b18ed
Tempest van Schaik <tevansch@microsoft.com>
2018-07-03 17:55:10 +0100

    Blank install script

--- Extensions/OK/WindowsVM/install.ps1
+++ Extensions/OK/WindowsVM/install.ps1
@@ -1,8 +1,8 @@
-conda create -y --name OKenv
-conda activate OKenv
-conda install -y pip
-pip install okpy
-pip install -r https://raw.githubusercontent.com/Cal-CS-61A-Staff/ok-client/master/requirements.txt
-conda install -y ipykernel
-python -m ipykernel install --user --name myenv --display-name "OKenv"
-conda install -y jupyter
+#conda create -y --name OKenv
+#conda activate OKenv
+#conda install -y pip
+#pip install okpy
+#pip install -r https://raw.githubusercontent.com/Cal-CS-61A-Staff/ok-client/master/requirements.txt
+#conda install -y ipykernel
+#python -m ipykernel install --user --name myenv --display-name "OKenv"
+#conda install -y jupyter
